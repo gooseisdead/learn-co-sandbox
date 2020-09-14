3e944dedcaba61a209fcfe5ca6db625fb7c82666
Mike Smith <gooseisdead@mac.com>
2020-09-14 01:09:57 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,5 @@
+array = ["a", "b", "c", 'd']
+count = 0 
+while count < array.length do
+  count += 1 
+end
---
+++ homeruns.rb
@@ -0,0 +1,12 @@
+bucket = ["ball 1, ball 2, ball 3, ball 4, ball 5, ball 6, ball 7"]
+
+balls_hit = bucket.size 
+homeruns_hit = 0 
+
+loop do
+  if balls_hit < homeruns_hit
+    puts "You hit homerun number #{bucket[homeruns_hit]
+  else
+  break
+end
+end
